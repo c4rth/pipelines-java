d4549172073068e62d2654165d17c179c7bb170e
Thierry Carels <thierry.carels@belfius.be>
2020-03-13 13:05:48 +0100

    pester scriptanalyzer

---
+++ tests/pester-tests.ps1
@@ -0,0 +1,13 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$here = Join-Path $here ".."
+$scriptDir = Join-Path $here "script"
+
+Describe 'PSScriptAnalyzer analysis' {    
+    $file = Join-Path $scriptDir  "playground.ps1"
+
+    $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path $file -Severity Warning
+    
+    It 'Should not return any violation' {
+        $ScriptAnalyzerResults | Should BeNullOrEmpty
+    }
+}  
