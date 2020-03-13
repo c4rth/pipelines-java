b17d817a4bbf008e13371163c1116c2fb828bbf9
Thierry Carels <thierry.carels@belfius.be>
2020-03-13 14:12:26 +0100

    tests

--- tests/pester.tests.ps1
+++ tests/pester.tests.ps1
@@ -2,16 +2,11 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $here = Join-Path $here ".."
 $scriptDir = Join-Path $here "script"
 
-# Import-Module ".\Export-NUnitXml.psm1" -Force
-
 Describe 'PSScriptAnalyzer analysis' {    
     $file = Join-Path $scriptDir  "playground.ps1"
 
-    $ScriptAnalyzerResult = Invoke-ScriptAnalyzer -Path $file -Severity Warning # -ExcludeRule PSAvoidUsingWriteHost
-    If ( $ScriptAnalyzerResult ) {  
-        $ScriptAnalyzerResultString = $ScriptAnalyzerResult | Out-String
-        Write-Warning $ScriptAnalyzerResultString
+    It 'File must exist' {
+        $file | Should -Exist
     }
-    # Export-NUnitXml -ScriptAnalyzerResult $ScriptAnalyzerResult -Path '.\ScriptAnalyzerResult.xml'
 
 }  
