4bd2465836653d53151e223b95ffd42aabaa7f1b
Thierry Carels <thierry.carels@belfius.be>
2020-03-26 10:55:41 +0100

    add param

--- tests/pester.tests.ps1
+++ tests/pester.tests.ps1
@@ -1,3 +1,15 @@
+Param(
+    [string]$parameter1,
+    [string]$parameter2
+)
+
+Write-Host "parameter 1 : " $parameter1
+Write-Host "parameter 2 : " $parameter2
+
+Get-InstalledModule
+
+Install-Module -Name AzModule -Force -ErrorAction Stop
+
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $here = Join-Path $here ".."
 $scriptDir = Join-Path $here "script"
