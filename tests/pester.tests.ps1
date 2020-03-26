46dc55f5469fb4c4c7260e1332f69716dcbb357a
Thierry Carels <thierry.carels@belfius.be>
2020-03-26 11:03:40 +0100

    fix module Az

--- tests/pester.tests.ps1
+++ tests/pester.tests.ps1
@@ -6,9 +6,11 @@ Param(
 Write-Host "parameter 1 : " $parameter1
 Write-Host "parameter 2 : " $parameter2
 
+Write-Host "Get-InstalledModule"
 Get-InstalledModule
 
-Install-Module -Name AzModule -Force -ErrorAction Stop
+Write-Host "Install-Module Az"
+Install-Module -Name Az -Force -ErrorAction Stop
 
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $here = Join-Path $here ".."
